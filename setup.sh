a1bf7cd8899e9cfe6cd4b60e9ee419b7dcc64b3d
Anatoly Bubenkov <bubenkoff@gmail.com>
2013-07-16 17:49:21 +0200

    add screen

--- setup.sh
+++ setup.sh
@@ -52,14 +52,28 @@ fi
 cd ~/.vim/bundle
 if ! [ -d "vim-ctrlp" ]
 then
-   git clone https://github.com/kien/ctrlp.vim.git
+   git clone https://github.com/kien/ctrlp.vim.git vim-ctrlp
 fi
 
 # setup nerdtree
 cd ~/.vim/bundle
 if ! [ -d "vim-nerdtree" ]
 then
-    git clone https://github.com/scrooloose/nerdtree
+    git clone https://github.com/scrooloose/nerdtree vim-nerdtree
+fi
+
+# setup nerdtree-tabs
+cd ~/.vim/bundle
+if ! [ -d "vim-nerdtree-tabs" ]
+then
+    git clone https://github.com/jistr/vim-nerdtree-tabs.git vim-nerdtree-tabs
+fi
+
+# setup vim-screen
+cd ~/.vim/bundle
+if ! [ -d "vim-screen" ]
+then
+    git clone https://github.com/ervandew/screen.git vim-screen
 fi
 
 # Set vimrc
